a5d26c2fbc36b659679572a6c29094c63dfd2a20
HalfSocioPath <fahad.rajpoot99@gmail.com>
2020-04-22 01:36:11 +0500

    Initial commit

---
+++ app/src/main/java/com/example/sqlitewithrecyclertask/DBModelClass.java
@@ -0,0 +1,22 @@
+package com.example.sqlitewithrecyclertask;
+
+public class DBModelClass
+{
+    private String name,address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
